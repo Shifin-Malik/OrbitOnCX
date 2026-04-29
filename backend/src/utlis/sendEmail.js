bc7b210902c494b899a5afb5dd50902e5898aa1e
Shifin-Malik <shifinmalik46@gmail.com>
2026-04-29 13:56:40 +0530

    fix: import dotenv and configure environment variables in sendEmail utility

--- backend/src/utlis/sendEmail.js
+++ backend/src/utlis/sendEmail.js
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer";
-
+import dotenv from 'dotenv';
+dotenv.config()
 export const sendEmail = async (email, subject, text, htmlContent = null) => {
   if (!process.env.EMAIL || !process.env.EMAIL_PASS) {
     console.error("Missing Email Credentials in .env file");
